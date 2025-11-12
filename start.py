c1438cd918698220aaa62c10b385f4c9f92c6f66
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2025-11-12 12:53:16 +0000

    Create the application start banner

---
+++ start.py
@@ -0,0 +1,24 @@
+"""Defines the main AiiDAlab app's start banner."""
+
+import ipywidgets as ipw
+
+from aiidalab_chemshell.common.navigation import QuickAccessButtons
+
+
+def get_start_widget(appbase, jupbase, notebase):
+    """Get the AiiDAlab app's start banner."""
+    logo = ipw.HTML(
+        f"""
+        <div class="app-container">
+            <a class="logo" href="{appbase}/main.ipynb" target="_blank">
+            <img src="{appbase}/images/alc.svg" alt="ALC AiiDAlab App Logo" />
+            </a>
+        </div>
+        """
+    )
+    return ipw.VBox(
+        children=[
+            logo,
+            QuickAccessButtons(),
+        ]
+    )
