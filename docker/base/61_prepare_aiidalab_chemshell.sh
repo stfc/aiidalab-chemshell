1f1702c3b082758449227220b51a40e3af868427
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2026-07-01 12:28:17 +0100

    Fix base docker container to match correct AiiDAlab app folder structure

--- docker/base/61_prepare_aiidalab_chemshell.sh
+++ docker/base/61_prepare_aiidalab_chemshell.sh
@@ -3,14 +3,16 @@
 # Install the AiiDAlab application 
 echo "INSTALLING AiiDAlab ChemShell Application"
 cd "${HOME}"/apps
-if [[ -d "aiidalab-chemshell" ]]; then 
+if [[ -d "chemshell" ]]; then 
     echo "AiiDAlab ChemShell app already installed..." 
 else 
-    pip install -e git+https://github.com/stfc/aiidalab-chemshell.git#egg=aiidalab-chemshell --src ${HOME}/apps
+    # pip install -e git+https://github.com/stfc/aiidalab-chemshell.git#egg=aiidalab-chemshell --src ${HOME}/apps
+    git clone -b v0.2.0 --depth 1  https://github.com/stfc/aiidalab-chemshell.git
+    mv aiidalab-chemshell chemshell 
 fi 
 
 # Install the associated python package 
-cd "${HOME}"/apps/aiidalab-chemshell 
+cd "${HOME}"/apps/chemshell 
 pip install -e . --no-user --no-cache-dir -q  
 
 # Return to the $HOME directory 
