8f593e65403753112715bbedadfc378f96029a5f
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2026-07-06 11:59:54 +0100

    Update extracted structure uploader widget

--- src/aiidalab_chemshell/common/structure_uploader.py
+++ src/aiidalab_chemshell/common/structure_uploader.py
@@ -1,17 +1,22 @@
 """Widget for selecting an input structure from various sources."""
 
-from aiida.orm import SinglefileData, StructureData
+from aiida.orm import SinglefileData, StructureData, TrajectoryData
 from aiidalab_widgets_base import SmilesWidget
 from ase import Atoms
-from ipywidgets import HTML, Tab, VBox
+from ipywidgets import HTML, Tab, VBox, dlink
+from traitlets import HasTraits, Instance
 
 from aiidalab_chemshell.common.database import AiiDADatabaseWidget
 from aiidalab_chemshell.common.file_handling import FileUploadWidget
 from aiidalab_chemshell.common.structure_viewer import StructureViewWidget
 
 
-class StructureSelectionWidget(VBox):
+class StructureSelectionWidget(VBox, HasTraits):
     """Widget for selecting an input structre from various sources."""
+
+    structure_data = Instance(StructureData, allow_none=True)
+    structure_file = Instance(SinglefileData, allow_none=True)
+    trajectory_data = Instance(TrajectoryData, allow_none=True)
 
     def __init__(self, **kwargs):
         """StructureSelectionWidget constructor."""
@@ -48,6 +53,8 @@ class StructureSelectionWidget(VBox):
         self.database_widget.observe(self._on_database_search, "data_object")
         self.smiles_widget.observe(self._on_smiles_generation, "structure")
 
+        dlink((self.file_uploader, "file"), (self, "structure_file"))
+
         return
 
     def _on_file_upload(self, change: dict) -> None:
@@ -65,7 +72,9 @@ class StructureSelectionWidget(VBox):
         """When SMILES string is inputted."""
         if change["new"] != change["old"]:
             self._create_viewer(change["new"])
-            # self.model.structure = StructureData(ase=change["new"])
+            self.structure = StructureData(ase=change["new"])
+            if self.structure_file:
+                self.structure_file = None
         return
 
     def _on_database_search(self, change: dict) -> None:
@@ -73,10 +82,14 @@ class StructureSelectionWidget(VBox):
         if change["new"] == change["old"]:
             return
         if isinstance(change["new"], SinglefileData):
-            # self.model.structure_file = change["new"]
+            if self.structure_data:
+                self.structure_data = None
+            self.structure_file = change["new"]
             self._on_file_upload(change)
         elif isinstance(change["new"], StructureData):
-            # self.model.structure = change["new"]
+            if self.structure_file:
+                self.structure_file = None
+            self.structure = change["new"]
             self._create_viewer(change["new"]._get_object_ase())
         else:
             self._create_viewer(None)
@@ -98,3 +111,11 @@ class StructureSelectionWidget(VBox):
             self.viewer,
         ]
         return
+
+    def disable(self, val: bool = True) -> None:
+        """Disable the widget and all children."""
+        for child in self.tabs.children:
+            try:
+                child.disable(True)
+            except AttributeError:
+                pass  # TODO: SmilesWidget cannot be easily disabled at present.
--- src/aiidalab_chemshell/wizards/structure.py
+++ src/aiidalab_chemshell/wizards/structure.py
@@ -1,13 +1,9 @@
 """Defines the model and view components for the structure setup stage."""
 
-import ase
 import ipywidgets as ipw
-from aiida.orm import SinglefileData, StructureData
-from aiidalab_widgets_base import SmilesWidget, WizardAppWidgetStep
+from aiidalab_widgets_base import WizardAppWidgetStep
 
-from aiidalab_chemshell.common.database import AiiDADatabaseWidget
-from aiidalab_chemshell.common.file_handling import FileUploadWidget
-from aiidalab_chemshell.common.structure_viewer import StructureViewWidget
+from aiidalab_chemshell.common.structure_uploader import StructureSelectionWidget
 from aiidalab_chemshell.models.structure import StructureInputModel
 
 
@@ -42,35 +38,15 @@ class StructureWizardStep(ipw.VBox, WizardAppWidgetStep):
             """
         )
 
-        self.tabs = ipw.Tab()
+        self.structure_uploader = StructureSelectionWidget()
 
-        # upload file
-        self.file_input_widget = ipw.VBox()
-        self.file_uploader = FileUploadWidget(description="Structure file: ")
-        self.file_input_widget.children = [
-            self.file_uploader,
-        ]
-        ipw.dlink((self.file_uploader, "file"), (self.model, "structure_file"))
-
-        # AiiDA database
-        self.database_widget = AiiDADatabaseWidget(
-            title="AiiDA Database",
-            query=[SinglefileData, StructureData],
+        ipw.dlink(
+            (self.structure_uploader, "structure_file"),
+            (self.model, "structure_file"),
         )
-
-        self.smiles_widget = SmilesWidget(title="SMILES")
-
-        self.tabs.children = [
-            self.file_input_widget,
-            self.database_widget,
-            self.smiles_widget,
-        ]
-        for i, title in enumerate(["Upload File", "AiiDA Database", "SMILES String"]):
-            self.tabs.set_title(i, title)
-
-        self.model.observe(self._on_file_upload, "structure_file")
-        self.database_widget.observe(self._on_database_search, "data_object")
-        self.smiles_widget.observe(self._on_smiles_generation, "structure")
+        ipw.dlink(
+            (self.structure_uploader, "structure_data"), (self.model, "structure")
+        )
 
     def render(self):
         """Render the wizard's contents if not already rendered."""
@@ -95,64 +71,17 @@ class StructureWizardStep(ipw.VBox, WizardAppWidgetStep):
     def _update_children(self) -> None:
         self.children = [
             self.info,
-            self.tabs,
-            ipw.HTML("<h2>Viewer:</h2>"),
-            self.viewer,
+            self.structure_uploader,
             self.submit_btn,
         ]
-        return
-
-    def _on_file_upload(self, change: dict) -> None:
-        """When file upload button is pressed."""
-        if self.model.has_file:
-            self.viewer = StructureViewWidget()
-            self.viewer.assign_structure_from_file(
-                self.model.structure_file.filename, self.model.structure_file.content
-            )
-            self._update_children()
-        return
-
-    def _on_smiles_generation(self, change: dict) -> None:
-        """When SMILES string is inputted."""
-        if change["new"] != change["old"]:
-            self._create_viewer(change["new"])
-            self.model.structure = StructureData(ase=change["new"])
-        return
-
-    def _on_database_search(self, change: dict) -> None:
-        """When data is loaded from AiiDA database."""
-        if change["new"] == change["old"]:
-            return
-        if isinstance(change["new"], SinglefileData):
-            self.model.structure_file = change["new"]
-            self._on_file_upload(change)
-        elif isinstance(change["new"], StructureData):
-            self.model.structure = change["new"]
-            self._create_viewer(change["new"]._get_object_ase())
-        else:
-            self._create_viewer(None)
-        return
-
-    def _create_viewer(self, structure: ase.Atoms | None) -> None:
-        """Create a viewer widget with the loaded ase.Atoms structure object."""
-        # if structure:
-        #     # self.viewer = awb.viewers.StructureDataViewer(structure=structure)
-        #     self.viewer = WeasWidget()
-        #     self.viewer.from_ase(structure)
-        # else:
-        #     self.viewer = ipw.HTML("<p>Could not visualise structure ...</p>")
-        self.viewer = StructureViewWidget()
-        self.viewer.assign_structure_from_ase(structure)
-        self._update_children()
         return
 
     def submit_structure(self, _):
         """Submit the structure step."""
         if self.model.has_file or self.model.has_structure:
-            self.file_uploader.disable(True)
-            self.database_widget.disable(True)
             self.submit_btn.disabled = True
             self.submit_btn.description = "Submitted"
+            self.structure_uploader.disable(True)
             self.model.submitted = True
         else:
             self.model.submitted = False
