3f73bc01783a39ce84b7b31b22c473c5b1127bf9
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2026-07-21 11:50:01 +0100

    Update StructureSelectionWidget to handle batch/trajectory inputs

--- src/aiidalab_chemshell/common/structure_uploader.py
+++ src/aiidalab_chemshell/common/structure_uploader.py
@@ -7,7 +7,6 @@ from alc_aiidalab_widgets.widgets import (
     FileUploadWidget,
     StructureViewWidget,
 )
-from ase import Atoms
 from ipywidgets import HTML, Tab, VBox, dlink
 from traitlets import HasTraits, Instance
 
@@ -19,61 +18,74 @@ class StructureSelectionWidget(VBox, HasTraits):
     structure_file = Instance(SinglefileData, allow_none=True)
     trajectory_data = Instance(TrajectoryData, allow_none=True)
 
-    def __init__(self, **kwargs):
-        """StructureSelectionWidget constructor."""
+    def __init__(self, batch: bool = False, **kwargs):
+        """
+        StructureSelectionWidget constructor.
+
+        Parameters
+        ----------
+        batch: bool
+            If true assume input will be a batch/trajectory style input
+        **kwargs :
+            Keyword arguments passed to the parent class's constructor.
+        """
         super().__init__(**kwargs)
+        self.batch = batch
         # upload file
         self.file_input_widget = VBox()
         self.file_uploader = FileUploadWidget(description="Structure file: ")
         self.file_input_widget.children = [
             self.file_uploader,
         ]
+        self.file_uploader.observe(self._on_file_upload, "file")
+        dlink((self.file_uploader, "file"), (self, "structure_file"))
 
         # AiiDA database
         self.database_widget = AiiDADatabaseQueryWidget(
             title="AiiDA Database",
-            query=[SinglefileData, StructureData],
+            query=[SinglefileData, StructureData, TrajectoryData]
+            if batch
+            else [SinglefileData, StructureData],
         )
+        self.database_widget.observe(self._on_database_search, "data_object")
 
-        self.smiles_widget = SmilesWidget(title="SMILES")
+        tabs_children = {
+            "Upload File": self.file_input_widget,
+            "AiiDA Database": self.database_widget,
+        }
+        if not self.batch:
+            self.smiles_widget = SmilesWidget(title="SMILES")
+            self.smiles_widget.observe(self._on_smiles_generation, "structure")
+            tabs_children["SMILES String"] = self.smiles_widget
 
         self.tabs = Tab()
-        self.tabs.children = [
-            self.file_input_widget,
-            self.database_widget,
-            self.smiles_widget,
-        ]
-        for i, title in enumerate(["Upload File", "AiiDA Database", "SMILES String"]):
+        self.tabs.children = [item for key, item in tabs_children.items()]
+        for i, title in enumerate(tabs_children.keys()):
             self.tabs.set_title(i, title)
 
         self.viewer = HTML("<p>No structure found...</p>")
 
-        self.children = [self.tabs, HTML("<h2>Viewer:</h2>"), self.viewer]
-
-        self.file_uploader.observe(self._on_file_upload, "file")
-        self.database_widget.observe(self._on_database_search, "data_object")
-        self.smiles_widget.observe(self._on_smiles_generation, "structure")
-
-        dlink((self.file_uploader, "file"), (self, "structure_file"))
+        self.children = [self.tabs, self.viewer]
 
         return
 
     def _on_file_upload(self, change: dict) -> None:
         """When file upload button is pressed."""
         if change["new"] != change["old"]:
-            self.viewer = StructureViewWidget()
-            self.viewer.assign_structure_from_file(
-                self.file_uploader.file.filename,
-                self.file_uploader.file.content,
-            )
+            self.viewer = StructureViewWidget(self.file_uploader.file)  # type: ignore
+            # self.viewer.assign_structure_from_file(
+            #     self.file_uploader.file.filename,
+            #     self.file_uploader.file.content,
+            # )
             self._update_children()
         return
 
     def _on_smiles_generation(self, change: dict) -> None:
         """When SMILES string is inputted."""
         if change["new"] != change["old"]:
-            self._create_viewer(change["new"])
             self.structure = StructureData(ase=change["new"])
+            self.viewer = StructureViewWidget(self.structure)
+            self._update_children()
             if self.structure_file:
                 self.structure_file = None
         return
@@ -85,25 +97,21 @@ class StructureSelectionWidget(VBox, HasTraits):
         if isinstance(change["new"], SinglefileData):
             if self.structure_data:
                 self.structure_data = None
+            if self.trajectory_data:
+                self.trajectory_data = None
             self.structure_file = change["new"]
-            self.viewer = StructureViewWidget(change["new"])
             self._update_children()
+        elif isinstance(change["new"], TrajectoryData):
+            if self.structure_file:
+                self.structure_file = None
+            if self.structure_data:
+                self.structure_data = None
+            self.trajectory_data = change["new"]
         elif isinstance(change["new"], StructureData):
             if self.structure_file:
                 self.structure_file = None
             self.structure = change["new"]
-            self._create_viewer(change["new"]._get_object_ase())
-        else:
-            self._create_viewer(None)
-        return
-
-    def _create_viewer(self, structure: Atoms | None) -> None:
-        """Create a viewer widget with the loaded ase.Atoms structure object."""
-        if structure:
-            self.viewer = StructureViewWidget()
-            self.viewer.assign_structure_from_ase(structure)
-        else:
-            self.viewer = HTML("<p>Could not visualise structure ...</p>")
+        self.viewer = StructureViewWidget(change["new"])
         self._update_children()
         return
 
